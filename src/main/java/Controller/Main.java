8448692ade30df7e9b2441f360b741ea26b7ee0d
Admin <Admin@DESKTOP-UG0GOA7>
2021-11-05 14:11:31 +0000

    Version 2.0 kata4 05/11/2021

--- src/main/java/Controller/Main.java
+++ src/main/java/Controller/Main.java
@@ -6,15 +6,41 @@ import Model.MailHistogramBuilder;
 import Model.MailListReader;
 import View.HistogramDisplay;
 import View.Mail;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+    private List<Mail> mailList;
+    private Histogram<String> histo;
+    private HistogramDisplay histogramDisplay;
+    private String fileName;
+
+    public Main(String fileName) {
+        this.fileName = fileName;
+    }
     public static void main(String[] args) {
-        String fileName = "C:\\Users\\Roberto GS\\Desktop\\IS2\\Kata4_RGS\\email.txt";
-        List<Mail> mailList = MailListReader.read(fileName);
-        Histogram<String> histo = MailHistogramBuilder.build(mailList);;
-        HistogramDisplay histogramDisplay= new HistogramDisplay(histo);
+        
+        Main main = new Main("C:\\Users\\Roberto GS\\Desktop\\IS2\\Kata4_RGS\\email.txt");
+        main.execute();
+        
+    }
+    
+    private void input(){
+        mailList = MailListReader.read(fileName);
+    }
+    
+    private void process(){
+        histo = MailHistogramBuilder.build(mailList);;
+        histogramDisplay= new HistogramDisplay(histo);
+    }
+    
+    private void output(){
         histogramDisplay.execute();
+    }
+
+    private void execute(){
+        input();
+        process();
+        output();
+        
     }
 }
